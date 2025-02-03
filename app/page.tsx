fc8da17c9bff3aefcb9236318d5fc5a1277b3330
Oscar Garcia <oscaregarcialeon@gmail.com>
2025-02-03 18:28:11 -0400

    Creacion de funcionalidad Autenticación con ClerkJS

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
+'use client'
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
 import Link from "next/link";
 
 export default function Home() {
+  const { toast } = useToast()
   return (
-    <div className="p-2">
+    <div className="p-2 flex items-center justify-around">
       <Button>
         <Link href="/admin">Cacha</Link>
       </Button>
+      <Button
+      onClick={() => {
+        toast({
+          title: "Scheduled: Catch up",
+          description: "Friday, February 10, 2023 at 5:57 PM",
+        })
+      }}
+    >
+      Show Toast
+    </Button>
     </div>
   );
 }
