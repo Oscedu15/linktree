83b25fb98f09ce56a97ea65b52fd6efd00dedc79
Oscar Garcia <oscaregarcialeon@gmail.com>
2025-02-06 16:16:32 -0400

    Creacion de Componente y funcionalidades de StepTwo

--- app/(routes)/(home)/components/StepTwo/StepTwo.data.ts
+++ app/(routes)/(home)/components/StepTwo/StepTwo.data.ts
@@ -1,42 +1,42 @@
 export const stepTwoData = [
   {
     icon: "https://w7jnmo36iy.ufs.sh/f/FI9IIQaHJdItYvtUNiPrlypHR1n9QfxUadjkV6XCINDuYBbJ",
-    name: "whatssap",
+    name: "WhatsApp",
     link: "",
   },
   {
     icon: "https://w7jnmo36iy.ufs.sh/f/FI9IIQaHJdIto4DK6rzlROvuXDkMKwqQ3FEACmfV4YbxieIt",
-    name: "instagram",
+    name: "Instagram",
     link: "",
   },
   {
     icon: "https://w7jnmo36iy.ufs.sh/f/FI9IIQaHJdItjNUpWvnDVFfPx3IkCbyYOZ68tA07Jp2uBD9S",
-    name: "facebook",
+    name: "Facebook",
     link: "",
   },
   {
     icon: "https://w7jnmo36iy.ufs.sh/f/FI9IIQaHJdItBISZ3VKmVzYoCfDKU3p50uElkZyr1LPJcWFs",
-    name: "twitter",
+    name: "Twitter",
     link: "",
   },
   {
     icon: "https://w7jnmo36iy.ufs.sh/f/FI9IIQaHJdItZJwjVVIudsOo0VC64ilqU5gWAR12e7XDhyar",
-    name: "linkedin",
+    name: "Linkedin",
     link: "",
   },
   {
     icon: "https://w7jnmo36iy.ufs.sh/f/FI9IIQaHJdItEOesNPBpS9e08yuUfkdNwZj16CH5QOYxRbiB",
-    name: "pinterest",
+    name: "Pinterest",
     link: "",
   },
   {
     icon: "https://w7jnmo36iy.ufs.sh/f/FI9IIQaHJdItmuR1cQAgQWOKMEL2VpdTv1cRwPtuyXSZrCJm",
-    name: "youtube",
+    name: "Youtube",
     link: "",
   },
   {
     icon: "https://w7jnmo36iy.ufs.sh/f/FI9IIQaHJdIt9V4fmZUt2WTaxjEwYQm86eNpSoq10RlDhn4k",
-    name: "tik tok",
+    name: "Tik Tok",
     link: "",
   },
 ];
--- app/(routes)/(home)/components/StepTwo/StepTwo.tsx
+++ app/(routes)/(home)/components/StepTwo/StepTwo.tsx
@@ -1,12 +1,40 @@
 import { Button } from "@/components/ui/button";
 import { useStepConfig } from "@/hooks/useStepConfig";
-import React from "react";
+import React, { useState } from "react";
 import { stepTwoData } from "./StepTwo.data";
 import Image from "next/image";
 
 export function StepTwo() {
   //Con useStepConfig traemos nuestro contexto de usuarios
   const { nextStep, infoUser, setInfoUser } = useStepConfig();
+
+  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>(
+    infoUser?.platforms?.map((platform) => platform.name) || []
+  );
+
+  const handleSelectPlatform = (platform: string) => {
+    setSelectedPlatforms((prevSelected) => {
+      if (prevSelected.includes(platform)) {
+        return prevSelected.filter((item) => item !== platform);
+      } else {
+        return [...prevSelected, platform];
+      }
+    });
+  };
+
+  console.log(selectedPlatforms);
+
+  const handleContinue = () => {
+    setInfoUser((prevInfoUser) => ({
+      ...prevInfoUser,
+
+      platforms: stepTwoData.filter(({ name }) =>
+        selectedPlatforms.includes(name)
+      ),
+    }));
+    nextStep();
+  };
+
   return (
     <div>
       <h2 className="text-center font-semibold text-2xl">
@@ -18,7 +46,13 @@ export function StepTwo() {
           <div
             key={index}
             className={`flex flex-col gap-1 items-center rounded-lg
-            py-3 hover:bg-violet-300 transition-all duration-300 cursor-pointer`}
+            py-3 hover:bg-violet-300 ${
+              selectedPlatforms.includes(data.name)
+                ? "bg-violet-900 text-white"
+                : "bg-slate-100 text-violet-900"
+            }
+             transition-all duration-300 cursor-pointer`}
+            onClick={() => handleSelectPlatform(data.name)}
           >
             <Image src={data.icon} alt="icon" width={40} height={40} />
             <p className="text-sm">{data.name}</p>
@@ -28,7 +62,7 @@ export function StepTwo() {
       <div className="mt-6">
         <Button
           className="w-full bg-purple-600 hover:bg-purple-700 transition-all duration-300 cursor-pointer"
-          onClick={nextStep}
+          onClick={handleContinue}
         >
           Continue
         </Button>
